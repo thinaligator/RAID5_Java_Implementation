ef9d3578b28782a9a7fe6d7d5b74438733bf4a03
piotokarczyk <piottok994@student.polsl.pl>
2024-05-23 11:05:47 +0200

    Initial commit

---
+++ src/Disc.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Cluster {
+    public static void main(String[] args) {
+
+
+        while(true){
+            System.out.println("insert bits you want to save in RAID5 data storage");
+            List<Boolean> list;
+
+        }
+
+
+    }
+
+
+
+}
