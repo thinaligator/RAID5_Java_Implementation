969333289d79233241dac981b0dd3dc454e5b3d6
piotokarczyk <piottok994@student.polsl.pl>
2024-05-24 18:25:10 +0200

    input validation and list<boolean> creation

--- src/Cluster.java
+++ src/Cluster.java
@@ -1,5 +1,39 @@
-public class Disc {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cluster{
+
+    public static void main(String[] args) {
+        Disc disc = new Disc(Disc.numOfDiscs(), Disc.bitSequence());
+        Cluster cluster = new Cluster();
+
+        System.out.println(cluster.toList(disc));
+
+
+
+    }
+
+
+    public static List<Boolean> toList(Disc disc){
+
+        String sequence = disc.getSequence();
+        List<Boolean> list = new ArrayList<>();
+
+        for (int i = 0; i < sequence.length(); i++) {
+            char character = sequence.charAt(i);
+            if(character == '1'){
+                list.add(true);
+            }else{
+                list.add(false);
+            }
+
+        }
+        return list;
+    }
+
+    public static void discCreate(int number){
+
+    }
 
 
 
--- src/Disc.java
+++ src/Disc.java
@@ -1,18 +1,57 @@
-import java.util.List;
+import java.util.Scanner;
 
-public class Cluster {
-    public static void main(String[] args) {
+public class Disc{
 
+    private int numberOfDiscs;
+    private String sequence;
 
-        while(true){
-            System.out.println("insert bits you want to save in RAID5 data storage");
-            List<Boolean> list;
-
-        }
-
-
+    public Disc(int numberOfDiscs, String sequence) {
+        this.numberOfDiscs = numberOfDiscs;
+        this.sequence = sequence;
     }
 
+    public int getNumberOfDiscs() {
+        return numberOfDiscs;
+    }
+    public String getSequence() {
+        return sequence;
+    }
+
+
+    public static String bitSequence(){
+        Scanner scanner = new Scanner(System.in);
+        String sequence="";
+        while (true) {
+            System.out.println("Please insert bit sequence you want to save in a RAID5 data storage");
+            sequence = scanner.nextLine();
+
+            if (sequence.matches("1[01]+")) {
+                System.out.println("Sequence accepted: " + sequence);
+                break;
+            } else {
+                System.out.println("Invalid sequence. The sequence must start with 1. Please try again.");
+            }
+        }
+        return sequence;
+    }
+
+    public static int numOfDiscs(){
+        Scanner scanner = new Scanner(System.in);
+        String stringNumberOfDiscs="";
+        while (true) {
+            System.out.println("Please insert number of discs you want to use in a RAID5 data storage");
+            stringNumberOfDiscs = scanner.nextLine();
+
+            if (stringNumberOfDiscs.matches("[3-9]")){
+                System.out.println("Number of discs accepted: " + stringNumberOfDiscs);
+                break;
+            } else {
+                System.out.println("Invalid number of discs. Minimum value is 3, maximum value is 9. Please try again.");
+            }
+        }
+        int number = Integer.parseInt(stringNumberOfDiscs);
+        return number;
+    }
 
 
 }
