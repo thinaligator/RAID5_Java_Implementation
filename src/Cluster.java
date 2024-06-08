4fd62b5a9344780f033af364aab87c1a8fd64e3d
Piotr Tokarczyk <130757006+thinaligator@users.noreply.github.com>
2024-06-08 03:15:30 +0200

    zapis dziala, zniszczenie dysku tez :))

--- src/Cluster.java
+++ src/Cluster.java
@@ -1,19 +1,128 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Cluster{
 
     public static void main(String[] args) {
         Disc disc = new Disc(Disc.numOfDiscs(), Disc.bitData());
+        Disc discNew = new Disc(Disc.damagedDiscIndex());
         Cluster cluster = new Cluster();
         List<List<Boolean>> listOfDiscs = cluster.createDiscList(disc);
 
         List<Boolean> list = cluster.toList(disc);
         saveData(list, listOfDiscs);
 
+        simulateDamage(discNew, listOfDiscs);
         for (int i = 0; i < listOfDiscs.size(); i++) {
             System.out.println("Disc " + i + ": " + listOfDiscs.get(i));
-        }
+        };
+//        readData(list, listOfDiscs);
+
+    }
+
+
+
+//    public static void readData(List<Boolean> list, List<List<Boolean>> listOfDiscs){
+//        int numberOfDiscs = listOfDiscs.size();
+//        int skipIndex = numberOfDiscs - 1;
+//        int dataSize = listOfDiscs.get(0).size();
+//        for (int i = 0; i < listOfDiscs.size(); i++) {
+//            System.out.println("Disc " + i + ": " + listOfDiscs.get(i));
+//        }
+//
+//
+//        for (int i = 0; i < dataSize; i++) {
+//            boolean parityBit = false;
+//            for (int j = 0; j < numberOfDiscs; j++) {
+//                if (j != skipIndex) {
+//                    boolean dataBit = listOfDiscs.get(j).get(i);
+//                    parityBit ^= dataBit;
+//                    parityBit = !parityBit;
+//                }
+//            }
+//            listOfDiscs.get(skipIndex).set(i, parityBit);
+//        }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//        int dataLength = list.size();
+//        int dataLength2 = list.size();
+//        int totalLength = dataLength + (numberOfDiscs - 1 - (dataLength % (numberOfDiscs - 1)));
+//
+////        for (int i = dataLength; i < totalLength; i++) {
+////            list.add(false);
+////        }
+//
+//        int currentIndex = 0;
+//        boolean decrementing = true;
+//
+//
+//        while (currentIndex < totalLength) {
+//            int currentIterationStartIndex = currentIndex;
+//            for (int i = 0; i < numberOfDiscs; i++) {
+//                if (currentIndex < totalLength) {
+//                    Boolean bit = list.get(currentIndex);
+//                    System.out.println(listOfDiscs.get(i)+"     "+i);
+//                    currentIndex++;
+//                }
+//            }
+//
+//            if (currentIndex == currentIterationStartIndex) {
+//                break;
+//            }
+//            //alg do przechodzenia dla sumie kontr
+//            if (decrementing) {
+//                skipIndex--;
+//                if (skipIndex < 0) {
+//                    skipIndex = 1;
+//                    decrementing = false;
+//                }
+//            } else {
+//                skipIndex++;
+//                if (skipIndex >= numberOfDiscs) {
+//                    skipIndex = numberOfDiscs - 2;
+//                    decrementing = true;
+//                }
+//            }
+//
+//        }
+////        if (dataLength2 % (listOfDiscs.size() -1) == 0) {
+////            for (List<Boolean> disc : listOfDiscs) {
+////                disc.remove(disc.size() - 1);
+////            }
+////        }
+//
+//        sumControl(listOfDiscs);
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void simulateDamage(Disc discNew, List<List<Boolean>> listOfDiscs) {
+        int discIndex = discNew.getDamagedDiscIndex();
+        listOfDiscs.remove(discIndex);
+        List<Boolean> newDisc = new ArrayList<>(Collections.nCopies(listOfDiscs.get(0).size(), false));
+
+        listOfDiscs.add(discIndex, newDisc);
     }
 
     public static void sumControl(List<List<Boolean>> listOfDiscs) {
